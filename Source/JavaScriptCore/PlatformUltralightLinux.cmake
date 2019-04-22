18fbc19c3b0f145b0495020fe2b909bc013d5f3b
Adam Simmons <hi@adamjsimmons.com>
2019-04-22 15:23:12 -0700

    Fix file permissions issue in build script.

--- Source/JavaScriptCore/PlatformUltralightLinux.cmake
+++ Source/JavaScriptCore/PlatformUltralightLinux.cmake
@@ -7,15 +7,25 @@ add_definitions(-DSTATICALLY_LINKED_WITH_WTF -D__STDC_WANT_LIB_EXT1__)
 
 file(MAKE_DIRECTORY ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore)
 
+set(BUILD_TMP "${CMAKE_BINARY_DIR}/tmp/")
+file(MAKE_DIRECTORY "${BUILD_TMP}")
+
 set(JavaScriptCore_PRE_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/JavaScriptCore/preBuild.sh")
-file(REMOVE "${JavaScriptCore_PRE_BUILD_COMMAND}")
-
-file(WRITE "${JavaScriptCore_PRE_BUILD_COMMAND}" "#!/bin/bash\ncp -R ${JAVASCRIPTCORE_DIR}/ForwardingHeaders/ ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
-foreach (_directory ${JavaScriptCore_FORWARDING_HEADERS_DIRECTORIES})
-file(APPEND "${JavaScriptCore_PRE_BUILD_COMMAND}" "rsync -aqW ${JAVASCRIPTCORE_DIR}/${_directory}/*.h ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
-endforeach ()
-
 set(JavaScriptCore_POST_BUILD_COMMAND "${CMAKE_BINARY_DIR}/DerivedSources/JavaScriptCore/postBuild.sh")
 
-file(WRITE "${JavaScriptCore_POST_BUILD_COMMAND}" "#!/bin/bash\nrsync -aqW ${DERIVED_SOURCES_DIR}/JavaScriptCore/*.h ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
-file(APPEND "${JavaScriptCore_POST_BUILD_COMMAND}" "rsync -aqW ${DERIVED_SOURCES_DIR}/JavaScriptCore/inspector/*.h ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
+# Write the pre-build bash script
+file(WRITE "${BUILD_TMP}/preBuild.sh" "#!/bin/bash\ncp -R ${JAVASCRIPTCORE_DIR}/ForwardingHeaders/ ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
+foreach (_directory ${JavaScriptCore_FORWARDING_HEADERS_DIRECTORIES})
+file(APPEND "${BUILD_TMP}/preBuild.sh" "rsync -aqW ${JAVASCRIPTCORE_DIR}/${_directory}/*.h ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
+endforeach ()
+
+# Write the post-build bash script
+file(WRITE "{BUILD_TMP}/postBuild.sh" "#!/bin/bash\nrsync -aqW ${DERIVED_SOURCES_DIR}/JavaScriptCore/*.h ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
+file(APPEND "{BUILD_TMP}/postBuild.sh" "rsync -aqW ${DERIVED_SOURCES_DIR}/JavaScriptCore/inspector/*.h ${DERIVED_SOURCES_DIR}/ForwardingHeaders/JavaScriptCore 2>/dev/null\n")
+
+# Copy bash scripts over and chmod to executable
+file (COPY "${BUILD_TMP}/preBuild.sh" DESTINATION "${CMAKE_BINARY_DIR}/DerivedSources/JavaScriptCore" FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
+file (COPY "${BUILD_TMP}/postBuild.sh" DESTINATION "${CMAKE_BINARY_DIR}/DerivedSources/JavaScriptCore" FILE_PERMISSIONS OWNER_EXECUTE OWNER_WRITE OWNER_READ)
+
+# Remove temp directory
+file(REMOVE_RECURSE "${BUILD_TMP}")
