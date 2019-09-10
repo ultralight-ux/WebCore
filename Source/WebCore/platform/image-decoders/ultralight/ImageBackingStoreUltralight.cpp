1f8a7d6244ef4b47f053d7f8a871582ec2b24d7d
Adam Simmons <hi@adamjsimmons.com>
2019-09-10 16:16:19 -0500

    Make ImageBackingStore use UltralightCore's sRGB gamma fixup routine when transporting bitmap.

--- Source/WebCore/platform/image-decoders/ultralight/ImageBackingStoreUltralight.cpp
+++ Source/WebCore/platform/image-decoders/ultralight/ImageBackingStoreUltralight.cpp
@@ -2,6 +2,7 @@
 #include "ImageBackingStore.h"
 #include <Ultralight/Bitmap.h>
 #include <Ultralight/private/Image.h>
+#include <ultralight/private/Paint.h>
 
 namespace WebCore {
 
@@ -15,19 +16,26 @@ NativeImagePtr ImageBackingStore::image(bool is_complete) const
   uint32_t height = size().height();
   uint32_t bpp = sizeof(RGBA32);
   static_assert(sizeof(RGBA32) == 4, "We expect sizeof(RGBA32) to be 4 on all platforms.");
-  ultralight::Ref<ultralight::Bitmap> bitmap = ultralight::Bitmap::Create(width, height, ultralight::kBitmapFormat_RGBA8,
-    width * bpp, (const void*)m_pixelsPtr, width * height * bpp, true);
+  ultralight::Ref<ultralight::Bitmap> bitmap = ultralight::Bitmap::Create(width, height, ultralight::kBitmapFormat_BGRA8_UNORM_SRGB,
+    width * bpp, (const void*)m_pixelsPtr, width * height * bpp, true, true);
 
-  //bitmap->WritePNG("test.png");
+  //
+  // We convert sRGB post-gamma premultiplication to sRGB pre-gamma premultiplication
+  //
+  // PNGs with alpha are decoded in sRGB color-space (actually 2.2 gamma, which
+  // is a very close approximation of sRGB gamma) with alpha multiplied with
+  // RGB channels AFTER the gamma function.
+  //
+  // To ensure our rendering pipeline is gamma-correct, we do all blending in
+  // linear color-space with all alpha values pre-multiplied linearly. This
+  // means we need to convert from sRGB POST-gamma premultiplied-alpha encoding
+  // to sRGB PRE-gamma premultiplied-alpha encoding to use the sRGB conversion
+  // hardware in GPU.
+  //
+  // See: <http://www.realtimerendering.com/blog/a-png-puzzle/>
+  //
 
   m_nativeImage->first->SetFrame(0, 1, bitmap, is_complete);
-
-  /*
-  if (is_complete) {
-    auto& pixels = const_cast<Vector<RGBA32>&>(m_pixels);
-    pixels.clear();
-  }
-  */
 
   return m_nativeImage;
 }
