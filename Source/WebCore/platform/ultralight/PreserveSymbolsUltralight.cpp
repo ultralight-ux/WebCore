a476f284a9a8b6258ed308125c86c540e0f2a274
V-X <v@nul.lu>
2019-09-24 14:13:48 -0500

    Add missing symbols
    https://github.com/ultralight-ux/Ultralight/issues/163

--- Source/WebCore/platform/ultralight/PreserveSymbolsUltralight.cpp
+++ Source/WebCore/platform/ultralight/PreserveSymbolsUltralight.cpp
@@ -17,6 +17,19 @@ WEBCORE_EXPORT void PreserveSymbols() {
   JSContextGetGlobalContext(nullptr);
   JSGlobalContextCopyName(nullptr);
   JSGlobalContextSetName(nullptr, nullptr);
+  JSObjectGetArrayBufferByteLength(nullptr, nullptr, nullptr);
+  JSObjectGetArrayBufferBytesPtr(nullptr, nullptr, nullptr);
+  JSObjectGetTypedArrayBuffer(nullptr, nullptr, nullptr);
+  JSObjectGetTypedArrayByteLength(nullptr, nullptr, nullptr);
+  JSObjectGetTypedArrayByteOffset(nullptr, nullptr, nullptr);
+  JSObjectGetTypedArrayBytesPtr(nullptr, nullptr, nullptr);
+  JSObjectGetTypedArrayLength(nullptr, nullptr, nullptr);
+  JSObjectMakeArrayBufferWithBytesNoCopy(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+  JSObjectMakeTypedArray(nullptr, nullptr, nullptr, nullptr);
+  JSObjectMakeTypedArrayWithArrayBuffer(nullptr, nullptr, nullptr, nullptr);
+  auto _ = JSObjectMakeTypedArrayWithArrayBufferAndOffset(nullptr, kJSTypedArrayTypeNone, nullptr, 0, 0, nullptr);
+  auto _ = JSObjectMakeTypedArrayWithBytesNoCopy(nullptr, kJSTypedArrayTypeNone, nullptr, 0, nullptr, nullptr, nullptr);
+  auto _ = JSValueGetTypedArrayType(nullptr, nullptr, nullptr);
 }
 
 }
