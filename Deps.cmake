cabdfead888a1622e17c55053102d6706dbcdd29
Adam Simmons <hi@adamjsimmons.com>
2020-05-18 23:05:01 -0500

    Update dependencies.

--- Deps.cmake
+++ Deps.cmake
@@ -23,7 +23,7 @@ elseif (PORT MATCHES "UltralightWin")
 endif ()
 
 set(WEBCORE_DEPS_REV "1.2")
-set(ULTRALIGHTCORE_REV "cd3e9e53")
+set(ULTRALIGHTCORE_REV "66cf9e25")
 
 if(${USE_LOCAL_DEPS})
   message("Using local deps.")
