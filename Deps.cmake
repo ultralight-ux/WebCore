0361c5f7c334e565cb6e2cac55d0d0a046bafb2a
Adam Simmons <hi@adamjsimmons.com>
2020-05-27 13:53:13 -0500

    Update dependencies, closes ultralight-ux/Ultralight#256

--- Deps.cmake
+++ Deps.cmake
@@ -23,7 +23,7 @@ elseif (PORT MATCHES "UltralightWin")
 endif ()
 
 set(WEBCORE_DEPS_REV "1.2")
-set(ULTRALIGHTCORE_REV "eb0f3cac")
+set(ULTRALIGHTCORE_REV "50f489d8")
 
 if(${USE_LOCAL_DEPS})
   message("Using local deps.")
