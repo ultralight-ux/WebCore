5540e17a7bb4b9dc899c09f6155ffc83529c453c
Adam Simmons <hi@adamjsimmons.com>
2020-06-01 14:02:33 -0500

    For better compatibility with UWP, only check if full file path exists when loading cacert.pem in SSL handle code.

--- Source/WebCore/platform/network/ultralight/CurlSSLHandleUltralight.cpp
+++ Source/WebCore/platform/network/ultralight/CurlSSLHandleUltralight.cpp
@@ -14,16 +14,10 @@ void CurlSSLHandle::platformInitialize()
   auto& platform = ultralight::Platform::instance();
   auto config = platform.config();
   WTF::String resource_path = ultralight::Convert(config.resource_path);
-  if (!FileSystem::fileIsDirectory(resource_path, FileSystem::ShouldFollowSymbolicLinks::Yes)) {
-    WTF::String err_msg = "Error loading cacert.pem, the following path set in Config::resource_path does not exist or is not a directory: " + resource_path;
-    UL_LOG_ERROR(ultralight::Convert(err_msg));
-    return;
-  }
-
   WTF::String path = FileSystem::pathByAppendingComponent(
     ultralight::Convert(config.resource_path), "cacert.pem");
   if (!FileSystem::fileExists(path)) {
-    WTF::String err_msg = "Error loading cacert.pem, the following path does not exist: " + path + "\nMake sure that Config::resource_path is set to the directory that contains cacert.pem.";
+    WTF::String err_msg = "Error loading cacert.pem, the following file path does not exist: " + path + "\nMake sure that Config::resource_path is set to the directory that contains cacert.pem.";
     UL_LOG_ERROR(ultralight::Convert(err_msg));
     return;
   }
