b2d71adfb5526673ef6a22d3ccb4448ecc696b55
Adam Simmons <hi@adamjsimmons.com>
2020-06-01 14:04:42 -0500

    Fix occasional heap corruption issue on UWP by disabling resource usage thread polling on Windows (improves performance anyways since we can query JavaScriptCore directly for JS heap statistics on an as-needed basis).

--- Source/WebCore/platform/ultralight/MemoryUtils.cpp
+++ Source/WebCore/platform/ultralight/MemoryUtils.cpp
@@ -5,10 +5,16 @@
 #include <Ultralight/private/util/Debug.h>
 #include <sstream>
 #include <string>
+#if !OS(DARWIN)
+#include "CommonVM.h"
+#include <JavaScriptCore/VM.h>
+#endif
 
 namespace WebCore {
 
+#if OS(DARWIN)
 static ResourceUsageData gData;
+#endif
 
 static std::string fmtBytes(uint64_t bytes) {
   char* suffix[] = { "B", "KB", "MB", "GB", "TB" };
@@ -28,13 +34,17 @@ static std::string fmtBytes(uint64_t bytes) {
 }
 
 MemoryUtils::MemoryUtils() {
+#if OS(DARWIN)
   ResourceUsageThread::addObserver(this, Memory, [this](const ResourceUsageData& data) {
     gData = data;
   });
+#endif
 }
 
 MemoryUtils::~MemoryUtils() {
+#if OS(DARWIN)
   ResourceUsageThread::removeObserver(this);
+#endif
 }
 
 #define PRINT_STATS(str, obj) \
@@ -43,14 +53,19 @@ MemoryUtils::~MemoryUtils() {
 void MemoryUtils::logMemoryStatistics() {
   std::ostringstream stream;
   stream << "Memory Usage (WebCore): " << std::endl;
-  PRINT_STATS("    JavaScript:          ", gData.categories[MemoryCategory::GCHeap].totalSize() + gData.categories[MemoryCategory::GCOwned].totalSize());
 #if OS(DARWIN)
+  PRINT_STATS("    JavaScript:          ", gData.categories[MemoryCategory::GCHeap].totalSize() + gData.categories[MemoryCategory::GCOwned].totalSize());
   // These statistics are only available on macOS at this time because of the ability to tag memory pages
   PRINT_STATS("    JavaScript JIT:      ", gData.categories[MemoryCategory::JSJIT].totalSize());
   PRINT_STATS("    Images:              ", gData.categories[MemoryCategory::Images].totalSize());
   PRINT_STATS("    Layers:              ", gData.categories[MemoryCategory::Layers].totalSize());
   PRINT_STATS("    Page:                ", gData.categories[MemoryCategory::bmalloc].totalSize() + gData.categories[MemoryCategory::LibcMalloc].totalSize());
   //PRINT_STATS("    Other:               ", Other);
+#else
+  JSC::VM* vm = &commonVM();
+  size_t currentGCHeapCapacity = vm->heap.blockBytesAllocated();
+  size_t currentGCOwnedExtra = vm->heap.extraMemorySize();
+  PRINT_STATS("    JavaScript:          ", currentGCHeapCapacity + currentGCOwnedExtra);
 #endif
   UL_LOG_INFO(stream.str().c_str());
 }
