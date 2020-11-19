c19c1980f4d7726777e459a3b0584c7eb562fd31
Adam Simmons <hi@adamjsimmons.com>
2020-11-18 18:05:55 -0600

    Update Windows build scripts, add options for no_jit, static_crt, debug_crt, static builds.

--- make.bat
+++ make.bat
@@ -18,7 +18,9 @@ if "%2"=="x64_uwp" (
 )
 GOTO SYNTAX
 :VALID
+
 set "DIRNAME=build_%1_%2"
+
 if "%1"=="vs" (
   set "FLAGS=-G "Visual Studio 16 2019" -A x64 -DCMAKE_BUILD_TYPE=RelWithDebInfo"
 )
@@ -34,27 +36,91 @@ if "%1"=="debug" (
 if "%1"=="full_debug" (
   set "FLAGS=-G "Ninja" -DCMAKE_BUILD_TYPE=Debug"
 )
+
+if "%2"=="x64_uwp" (
+  echo Using UWP Platform.
+  set "FLAGS=%FLAGS% -DUWP_PLATFORM=1 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0"
+) else (
+  set "FLAGS=%FLAGS% -DUWP_PLATFORM=0 -DWINDOWS_DESKTOP_PLATFORM=1"
+)
+
+set STATIC_BUILD=0
+set STATIC_CRT=0
+set DEBUG_CRT=0
+set USE_LOCAL_DEPS=0
+set NO_JIT=0
+
+:PROCESS_ARG
+if not "%3" == "" (
+  if "%3"=="static" ( 
+    set STATIC_BUILD=1
+  )
+  if "%3"=="static_crt" ( 
+    set STATIC_CRT=1
+  )
+  if "%3"=="debug_crt" ( 
+    set DEBUG_CRT=1
+  )
+  if "%3"=="local" (
+    set USE_LOCAL_DEPS=1
+  )
+  if "%3"=="no_jit" (
+    set NO_JIT=1
+  )
+  shift
+  goto PROCESS_ARG
+)
+
+if %STATIC_BUILD%==1 (
+  echo Building static library.
+  set "DIRNAME=%DIRNAME%_static"
+) else (
+  set "DIRNAME=%DIRNAME%_dll"
+)
+set "FLAGS=%FLAGS% -DSTATIC_BUILD=%STATIC_BUILD%"
+
+if %STATIC_CRT%==1 (
+  echo Building with static CRT
+  set "DIRNAME=%DIRNAME%_MT"
+) else (
+  set "DIRNAME=%DIRNAME%_MD"
+)
+set "FLAGS=%FLAGS% -DSTATIC_CRT=%STATIC_CRT%"
+
+if %DEBUG_CRT%==1 (
+  echo Building with debug CRT
+  set "DIRNAME=%DIRNAME%d"
+)
+set "FLAGS=%FLAGS% -DDEBUG_CRT=%DEBUG_CRT%"
+
+if %USE_LOCAL_DEPS%==1 (
+  echo Using local dependencies.
+) else (
+  echo Fetching dependencies from remote server.
+)
+set "FLAGS=%FLAGS% -DUSE_LOCAL_DEPS=%USE_LOCAL_DEPS%"
+
+if %NO_JIT%==1 (
+  echo Disabling JavaScriptCore JIT.
+  set "DIRNAME=%DIRNAME%_no_jit"
+) else (
+  echo Enabling JavaScriptCore JIT.
+)
+set "FLAGS=%FLAGS% -DNO_JIT=%NO_JIT%"
+
 call "%VCVARS%" %CFG%
 if not exist %DIRNAME% mkdir %DIRNAME%
 cd %DIRNAME%
 set CC=cl.exe
 set CXX=cl.exe
-set "EXTRA_FLAGS=-DUWP_PLATFORM=0 -DWINDOWS_DESKTOP_PLATFORM=1"
-if "%2"=="x64_uwp" (
-  echo Using UWP Platform.
-  set "EXTRA_FLAGS=-DUWP_PLATFORM=1 -DWINDOWS_DESKTOP_PLATFORM=0 -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0"
-)
-if "%3"=="local" (
-  echo Using local deps.
-  set "EXTRA_FLAGS=%EXTRA_FLAGS% -DUSE_LOCAL_DEPS=1"
-)
-cmake .. %FLAGS% %EXTRA_FLAGS%
+
+cmake .. %FLAGS%
 if "%1"=="vs" GOTO FINISH
 ninja
 GOTO FINISH
 :SYNTAX
 echo.
-echo usage: make [ release ^| full_release ^| debug ^| full_debug ^| vs ]  [ x64 ^| x64_uwp ] [ local ]
+echo usage: make [ release ^| full_release ^| debug ^| full_debug ^| vs ]  [ x64 ^| x64_uwp ] [ local ] [ static ] [ static_crt ]
 echo.
 echo Build type parameter descriptions:
 echo.
@@ -69,9 +135,12 @@ echo.
 echo     x64         Compile binaries for the x64 (amd64) platform.
 echo     x64_uwp     Compile binaries for the x64 (amd64) platform with UWP toolchain.
 echo.
-echo Optional third parameter:
+echo Additional, optional build options:
 echo.
-echo     local       Optional third parameter to use local dependencies in deps folder.
-echo.
+echo     local       Use local dependencies in deps folder (don't fetch from server).
+echo     static      Whether or not to build a static library. Disabled by default (creates a DLL).
+echo     static_crt  Whether or not to use static runtime library (MT). Disabled by default (MD instead).
+echo     debug_crt   Whether or not to use the debug runtime library (MDd or MTd, based on static_crt). Disabled by default.
+echo     no_jit      Whether or not to build without JavaScriptCore JIT (creates smaller build). Disabled by default.
 :FINISH
 cd ..
