a06825a0599f87aece1ea921c9217cd76581329f
Adam Simmons <hi@adamjsimmons.com>
2021-07-01 23:26:33 -0500

    Fix build errors on macOS and Linux.

--- Source/cmake/OptionsUltralightLinux.cmake
+++ Source/cmake/OptionsUltralightLinux.cmake
@@ -14,32 +14,11 @@ set(USE_GLIB 1)
 if (${BUILD_DBG})
   # Debug Config
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -DNDEBUG -fPIC")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -DNDEBUG -ffunction-sections -fdata-sections -fPIC -std=c++14
-                       -Wno-trigraphs -fno-exceptions -Wno-missing-field-initializers -Wnon-virtual-dtor
-                       -Wno-overloaded-virtual -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses 
-                       -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable
-                       -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow
-                       -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion
-                       -Wbool-conversion -Wenum-conversion -Wno-float-conversion
-                       -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare
-                       -Wno-shorten-64-to-32 -Wno-c++11-extensions -Wdeprecated-declarations
-                       -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics -Wno-sign-conversion
-                       -Winfinite-recursion -Wno-strict-prototypes -DU_SHOW_CPLUSPLUS_API=0")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g -DNDEBUG -ffunction-sections -fdata-sections -fPIC -std=c++14 -Wno-trigraphs -fno-exceptions -Wno-missing-field-initializers -Wnon-virtual-dtor -Wno-overloaded-virtual -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses  -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion -Wbool-conversion -Wenum-conversion -Wno-float-conversion -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare -Wno-shorten-64-to-32 -Wno-c++11-extensions -Wdeprecated-declarations -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics -Wno-sign-conversion -Winfinite-recursion -Wno-strict-prototypes -DU_SHOW_CPLUSPLUS_API=0")
 else ()
   # Release Config
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Oz -DNDEBUG -fPIC")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=core2 -mtune=haswell -Oz -DNDEBUG -ffunction-sections
-                       -fdata-sections -fPIC -std=c++14 -Wno-trigraphs -fno-exceptions
-                       -Wno-missing-field-initializers -Wnon-virtual-dtor -Wno-overloaded-virtual
-                       -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses -Wswitch
-                       -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable
-                       -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow
-                       -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion
-                       -Wbool-conversion -Wenum-conversion -Wno-float-conversion
-                       -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare
-                       -Wno-shorten-64-to-32 -Wno-c++11-extensions -Wdeprecated-declarations
-                       -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics -Wno-sign-conversion
-                       -Winfinite-recursion -Wno-strict-prototypes -DU_SHOW_CPLUSPLUS_API=0")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=core2 -mtune=haswell -Oz -DNDEBUG -ffunction-sections -fdata-sections -fPIC -std=c++14 -Wno-trigraphs -fno-exceptions -Wno-missing-field-initializers -Wnon-virtual-dtor -Wno-overloaded-virtual -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion -Wbool-conversion -Wenum-conversion -Wno-float-conversion -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare -Wno-shorten-64-to-32 -Wno-c++11-extensions -Wdeprecated-declarations -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics -Wno-sign-conversion -Winfinite-recursion -Wno-strict-prototypes -DU_SHOW_CPLUSPLUS_API=0")
 endif ()
 
 # Allow relocatable binaries on Linux, load from executable path
--- Source/cmake/OptionsUltralightMac.cmake
+++ Source/cmake/OptionsUltralightMac.cmake
@@ -18,32 +18,9 @@ set(SDK_SYSROOT "/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.pla
 if (${BUILD_DBG})
   # Debug Config
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -g -DNDEBUG -fPIC")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch x86_64 -g -DNDEBUG -fPIC -std=gnu++14 -stdlib=libc++
-                       -Wno-trigraphs -fno-exceptions -Wno-missing-field-initializers -Wnon-virtual-dtor
-                       -Wno-overloaded-virtual -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses
-                       -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable
-                       -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow
-                       -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion
-                       -Wbool-conversion -Wenum-conversion -Wno-float-conversion
-                       -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare
-                       -Wno-shorten-64-to-32 -Wno-c++11-extensions -isysroot ${SDK_SYSROOT}
-                       -Wdeprecated-declarations -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics
-                       -Wno-sign-conversion -Winfinite-recursion -Wmove -Wno-comma -Wblock-capture-autoreleasing
-                       -Wno-strict-prototypes -Wno-nonportable-include-path -frtti")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch x86_64 -g -DNDEBUG -fPIC -std=gnu++14 -stdlib=libc++ -Wno-trigraphs -fno-exceptions -Wno-missing-field-initializers -Wnon-virtual-dtor -Wno-overloaded-virtual -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion -Wbool-conversion -Wenum-conversion -Wno-float-conversion -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare -Wno-shorten-64-to-32 -Wno-c++11-extensions -isysroot ${SDK_SYSROOT} -Wdeprecated-declarations -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics -Wno-sign-conversion -Winfinite-recursion -Wmove -Wno-comma -Wblock-capture-autoreleasing -Wno-strict-prototypes -Wno-nonportable-include-path -frtti")
 else ()
   # Release Configs
   set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Oz -DNDEBUG -fPIC")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch x86_64 -march=core2 -mtune=haswell -Oz -DNDEBUG -fPIC
-                       -std=gnu++14 -stdlib=libc++ -Wno-trigraphs -fno-exceptions
-                       -Wno-missing-field-initializers -Wnon-virtual-dtor -Wno-overloaded-virtual
-                       -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses -Wswitch
-                       -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable
-                       -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow
-                       -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion
-                       -Wbool-conversion -Wenum-conversion -Wno-float-conversion
-                       -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare
-                       -Wno-shorten-64-to-32 -Wno-c++11-extensions -isysroot ${SDK_SYSROOT}
-                       -Wdeprecated-declarations -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics
-                       -Wno-sign-conversion -Winfinite-recursion -Wmove -Wno-comma -Wblock-capture-autoreleasing
-                       -Wno-strict-prototypes -Wno-nonportable-include-path -frtti")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -arch x86_64 -march=core2 -mtune=haswell -Oz -DNDEBUG -fPIC -std=gnu++14 -stdlib=libc++ -Wno-trigraphs -fno-exceptions -Wno-missing-field-initializers -Wnon-virtual-dtor -Wno-overloaded-virtual -Wno-exit-time-destructors -Wno-missing-braces -Wparentheses -Wswitch -Wunused-function -Wno-unused-label -Wno-unused-parameter -Wunused-variable -Wunused-value -Wempty-body -Wuninitialized -Wno-unknown-pragmas -Wno-shadow -Wno-four-char-constants -Wno-conversion -Wconstant-conversion -Wint-conversion -Wbool-conversion -Wenum-conversion -Wno-float-conversion -Wnon-literal-null-conversion -Wobjc-literal-conversion -Wsign-compare -Wno-shorten-64-to-32 -Wno-c++11-extensions -isysroot ${SDK_SYSROOT} -Wdeprecated-declarations -Winvalid-offsetof -fvisibility=hidden -fno-threadsafe-statics -Wno-sign-conversion -Winfinite-recursion -Wmove -Wno-comma -Wblock-capture-autoreleasing -Wno-strict-prototypes -Wno-nonportable-include-path -frtti")
 endif ()
