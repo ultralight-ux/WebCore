e6c3f24e733b218dfdf39b31778a2e77faceac4c
Adam Simmons <adam@ultralig.ht>
2022-05-24 21:13:43 -0500

    Update deps.

--- Deps.cmake
+++ Deps.cmake
@@ -21,7 +21,7 @@ endif ()
 
 set(WEBCORE_DEPS_REV "00d24413")
 set(GSTREAMER_REV "288fedcc")
-set(ULTRALIGHTCORE_REV "e630aae6")
+set(ULTRALIGHTCORE_REV "3e5d8b96")
 
 if (${ALLINONE_BUILD})
   message("Using local deps from all-in-one build.")
