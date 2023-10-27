c092c46dceba39ea05aa77128342355829e684f2
Adam Simmons <adam@ultralig.ht>
2023-10-27 18:20:30 -0500

    Update macOS toolchain.

--- cmake/toolchains/clang_arm64_macos_11.3.cmake
+++ cmake/toolchains/clang_arm64_macos_11.3.cmake
@@ -1,7 +1,7 @@
-if (DEFINED TOOLCHAIN_ARM64_MACOS_11_0_CMAKE)
+if (DEFINED TOOLCHAIN_ARM64_MACOS_11_3_CMAKE)
   return ()
 else ()
-  set(TOOLCHAIN_ARM64_MACOS_11_0_CMAKE 1)
+  set(TOOLCHAIN_ARM64_MACOS_11_3_CMAKE 1)
 endif ()
 
 set(CMAKE_SYSTEM_NAME Darwin CACHE INTERNAL "" FORCE)
@@ -11,7 +11,7 @@ set(UNIX TRUE CACHE INTERNAL "" FORCE)
 set(APPLE TRUE CACHE INTERNAL "" FORCE)
 set(MSVC FALSE CACHE INTERNAL "" FORCE)
 
-set(triple arm64-apple-macos11)
+set(triple arm64-apple-macos11.3)
 
 set(CMAKE_C_COMPILER_TARGET ${triple} CACHE INTERNAL "" FORCE)
 set(CMAKE_CXX_COMPILER_TARGET ${triple} CACHE INTERNAL "" FORCE)
@@ -27,8 +27,8 @@ string(REGEX MATCH "^([0-9]+)\\.([0-9]+)" CLANG_VERSION_MATCH ${CLANG_VERSION_ST
 set(CLANG_VERSION_MAJOR ${CMAKE_MATCH_1})
 set(CLANG_VERSION_MINOR ${CMAKE_MATCH_2})
 
-set(CMAKE_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk"      CACHE PATH    "(macOS only) The path to the macOS platform SDK." FORCE)
-set(CMAKE_OSX_DEPLOYMENT_TARGET "11.0"                                           CACHE STRING  "(macOS only) The minimum macOS deployment target." FORCE)
+set(CMAKE_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX11.3.sdk"      CACHE PATH    "(macOS only) The path to the macOS platform SDK." FORCE)
+set(CMAKE_OSX_DEPLOYMENT_TARGET "11.3"                                           CACHE STRING  "(macOS only) The minimum macOS deployment target." FORCE)
 set(CMAKE_POSITION_INDEPENDENT_CODE CACHE BOOL ON FORCE)
 set(PORT "UltralightMac" CACHE INTERNAL "" FORCE)
 set(CMAKE_OSX_SYSROOT ${CMAKE_SYSROOT} CACHE PATH "(macOS only) The path to the macOS platform SDK." FORCE)
@@ -39,7 +39,7 @@ if (NOT EXISTS ${CMAKE_SYSROOT})
 endif ()
 
 set(SKIA_CPU "arm64")
-set(UL_SKIA_CFLAGS -arch arm64 -mmacosx-version-min=11.0)
+set(UL_SKIA_CFLAGS -arch arm64 -mmacosx-version-min=11.3)
 
 # Set up FFmpeg args
 #set(UL_FFMPEG_ARGS --target-os=darwin --enable-asm --cpu=haswell --sysroot=${CMAKE_SYSROOT})
--- cmake/toolchains/clang_x64_macos_11.3.cmake
+++ cmake/toolchains/clang_x64_macos_11.3.cmake
@@ -1,7 +1,7 @@
-if (DEFINED TOOLCHAIN_X64_MACOS_11_0_CMAKE)
+if (DEFINED TOOLCHAIN_X64_MACOS_11_3_CMAKE)
   return ()
 else ()
-  set(TOOLCHAIN_X64_MACOS_11_0_CMAKE 1)
+  set(TOOLCHAIN_X64_MACOS_11_3_CMAKE 1)
 endif ()
 
 find_program(CMAKE_C_COMPILER clang)
@@ -14,10 +14,10 @@ string(REGEX MATCH "^([0-9]+)\\.([0-9]+)" CLANG_VERSION_MATCH ${CLANG_VERSION_ST
 set(CLANG_VERSION_MAJOR ${CMAKE_MATCH_1})
 set(CLANG_VERSION_MINOR ${CMAKE_MATCH_2})
 
-set(CMAKE_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX11.0.sdk"      CACHE PATH    "(macOS only) The path to the macOS platform SDK.")
-set(CMAKE_OSX_DEPLOYMENT_TARGET "11.0"                                           CACHE STRING  "(macOS only) The minimum macOS deployment target.")
+set(CMAKE_SYSROOT "/Library/Developer/CommandLineTools/SDKs/MacOSX11.3.sdk"      CACHE PATH    "(macOS only) The path to the macOS platform SDK.")
+set(CMAKE_OSX_DEPLOYMENT_TARGET "11.3"                                           CACHE STRING  "(macOS only) The minimum macOS deployment target.")
 set(CMAKE_POSITION_INDEPENDENT_CODE CACHE BOOL ON)
-set(PORT "UltralightMac")
+set(PORT "UltralightMac" CACHE INTERNAL "" FORCE)
 set(CMAKE_OSX_SYSROOT ${CMAKE_SYSROOT} CACHE PATH "(macOS only) The path to the macOS platform SDK." FORCE)
 set(CMAKE_OSX_ARCHITECTURES "x86_64" CACHE STRING "" FORCE)
 
@@ -25,7 +25,7 @@ if (NOT EXISTS ${CMAKE_SYSROOT})
     message(FATAL_ERROR "macOS Platform SDK not found at the following path: ${CMAKE_SYSROOT}")
 endif ()
 
-set(UL_SKIA_CFLAGS -arch x86_64 -mmacosx-version-min=11.0)
+set(UL_SKIA_CFLAGS -arch x86_64 -mmacosx-version-min=11.3)
 
 # Set up FFmpeg args
 #set(UL_FFMPEG_ARGS --target-os=darwin --enable-asm --cpu=haswell --sysroot=${CMAKE_SYSROOT})
